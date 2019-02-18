548d6b4b57e3946c8a79e0fd1e448cb3a24f931c
Sheba Mathew <sheba.m.mathew@gmail.com>
2019-02-17 21:00:12 -0500

    added validation to addNote

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,22 +1,105 @@
 import React from 'react'; 
+import ValidationError from '../ValidationError'; 
 
 export default class AddNote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '', 
+            content: '',  
+            folder: '',
+            titleValid: false, 
+            formValid: false, 
+            validationMessages: {
+                title: '', 
+            }
+        }
+    }
+    
+    handleSubmit(event) {
+        event.preventDefault(); 
+        const { title, content, folder } = this.state; 
+        console.log('Title: ', title); 
+        console.log('Content: ', content); 
+        console.log('Folder: ', folder); 
+    } 
+
+    updateTitle(title) {
+        this.setState({title}, () => {this.validateTitle(title)});
+    }
+      
+    updateContent(content) {
+        this.setState({content});
+    }
+      
+    updateFolder(folder) {
+        this.setState({folder});
+    }
+
+    validateTitle(fieldValue) {
+        const fieldErrors = {...this.state.validationMessages};
+        let hasError = false;
+    
+        fieldValue = fieldValue.trim();
+        if(fieldValue.length === 0) {
+          fieldErrors.title = 'Title is required';
+          hasError = true;
+        } else {
+            fieldErrors.title = '';
+            hasError = false;
+        }
+    
+        this.setState({
+          validationMessages: fieldErrors,
+            titleValid: !hasError
+        }, this.formValid );
+    
+    }
+
+    formValid() {
+        this.setState({
+          formValid: this.state.titleValid 
+        });
+    }
+
+    folderOptions(folders) {
+		let options;
+		if (folders.length === 0) {
+			options = <option value="">Loading...</option>;
+		} else {
+			options = folders.map(folder => (
+				<option key={folder.id} value={folder.id}>
+					{folder.name}
+				</option>
+			));
+			options.unshift(
+				<option key="select" value="">
+					Select a folder...
+				</option>
+			);
+		}
+		return options;
+	}
+
     render() {
         return (
-            <form>
+            <form className="addNoteForm" onSubmit={e => this.handleSubmit(e)}>
                 <h2>Create a Note</h2>
                 <div>* required field</div>  
                 <div className="form-group">
                     <label htmlFor="title">Title *</label>
-                    <input type="text" id="Title"/>
+                    <input type="text" id="Title" onChange={e => this.updateTitle(e.target.value)}/>
+                    <ValidationError hasError={!this.state.titleValid} message={this.state.validationMessages.title}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="Content">Content *</label>
-                    <textarea id="content"></textarea>
+                    <textarea id="content" onChange={e => this.updateContent(e.target.value)}></textarea>
                 </div>
                 <div className="form-group">
                     <label htmlFor="Folder">Folder</label>
-                    <select id="folderSelect"/>
+                    <select id="folderSelect" onChange={e => this.updateFolder(e.target.value)}>
+                        {this.folderOptions}
+                    </select>
                 </div>
 
                 <div>
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Header from '../Header/Header'; 
 import Sidebar from '../Sidebar/Sidebar'; 
 import Main from '../Main/Main'; 
+import NotefulContext from '../NotefulContext'; 
 
 export default class HomePage extends React.Component {
+    static contextType = NotefulContext; 
+
     render() {
         return (
             <div className="folderPage">
                 <Header/>
                 <Sidebar/>
-                <Main/>
+                <Main /> 
             </div>
         )
     }
