4fa06d7fee54e093a9af0e028e714351eaf81df5
Sheba Mathew <sheba.m.mathew@gmail.com>
2019-02-17 19:17:55 -0500

    created addNote form

---
+++ src/AddNote/AddNote.js
@@ -0,0 +1,30 @@
+import React from 'react'; 
+
+class AddFolder extends React.Component {
+    render() {
+        return (
+            <form>
+                <h2>Create a Note</h2>
+                <div>* required field</div>  
+                <div className="form-group">
+                    <label htmlFor="title">Title *</label>
+                    <input type="text" id="Title"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="Content">Content *</label>
+                    <textarea id="content"></textarea>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="Folder">Folder</label>
+                    <select id="folderSelect"/>
+                </div>
+
+                <div>
+                    <button type="submit" className="form_button">
+                        Add Note 
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}
